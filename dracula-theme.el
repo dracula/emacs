11391ea531d40fb08c64313bbb86e4d29d7fe1c5
Étienne Deparis <etienne@depar.is>
2020-04-04 16:14:40 +0200

    Try a new 256 colors palette
    This new palette comes from https://github.com/dracula/dracula-theme/pull/264
    
    Other colors are taken from here: https://jonasjacek.github.io/colors/

--- dracula-theme.el
+++ dracula-theme.el
@@ -78,25 +78,25 @@ read it before opening a new issue about your will.")
 
 ;; Assigment form: VARIABLE COLOR [256-COLOR [TTY-COLOR]]
 (let ((colors '(;; Upstream theme color
-                (dracula-bg      "#282a36" "#262626" nil) ; official background
+                (dracula-bg      "#282a36" nil       nil) ; official background
                 (dracula-fg      "#f8f8f2" "#ffffff" "brightwhite") ; official foreground
-                (dracula-current "#44475a" "#262626" "brightblack") ; official current-line/selection
-                (dracula-comment "#6272a4" "#7a7a7a" "blue")        ; official comment
-                (dracula-cyan    "#8be9fd" "#88eeff" "brightcyan")  ; official cyan
-                (dracula-green   "#50fa7b" "#55ff77" "green")       ; official green
-                (dracula-orange  "#ffb86c" "#ffbb66" "brightred")   ; official orange
-                (dracula-pink    "#ff79c6" "#ff77cc" "magenta")     ; official pink
-                (dracula-purple  "#bd93f9" "#bb99ff" "brightmagenta") ; official purple
-                (dracula-red     "#ff5555" "#ff6655" "red")         ; official red
-                (dracula-yellow  "#f1fa8c" "#ffff88" "yellow")      ; official yellow
+                (dracula-current "#44475a" "#303030" "brightblack") ; official current-line/selection
+                (dracula-comment "#6272a4" "#5f5faf" "blue")        ; official comment
+                (dracula-cyan    "#8be9fd" "#87d7ff" "brightcyan")  ; official cyan
+                (dracula-green   "#50fa7b" "#5fff87" "green")       ; official green
+                (dracula-orange  "#ffb86c" "#ffaf5f" "brightred")   ; official orange
+                (dracula-pink    "#ff79c6" "#ff87d7" "magenta")     ; official pink
+                (dracula-purple  "#bd93f9" "#af87ff" "brightmagenta") ; official purple
+                (dracula-red     "#ff5555" "#ff8787" "red")         ; official red
+                (dracula-yellow  "#f1fa8c" "#ffff87" "yellow")      ; official yellow
                 ;; Other colors
-                (bg2             "#373844" "#2e2e2e" "brightblack")
+                (bg2             "#373844" "#121212" "brightblack")
                 (bg3             "#464752" "#262626" "brightblack")
-                (bg4             "#565761" "#3f3f3f" "brightblack")
-                (fg2             "#e2e2dc" "#bfbfbf" "brightwhite")
-                (fg3             "#ccccc7" "#cccccc" "white")
-                (fg4             "#b6b6b2" "#bbbbbb" "white")
-                (other-blue      "#0189cc" "#0088cc" "brightblue")))
+                (bg4             "#565761" "#444444" "brightblack")
+                (fg2             "#e2e2dc" "#e4e4e4" "brightwhite")
+                (fg3             "#ccccc7" "#c6c6c6" "white")
+                (fg4             "#b6b6b2" "#b2b2b2" "white")
+                (other-blue      "#0189cc" "#0087ff" "brightblue")))
       (faces '(;; default
                (cursor :background ,fg3)
                (completions-first-difference :foreground ,dracula-pink :weight bold)
