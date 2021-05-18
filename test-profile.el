18846c490137127b0e0d2a45e361730a795ec97c
Étienne Deparis <etienne@depar.is>
2021-05-18 08:52:46 +0200

    Little change to test files

--- test-profile.el
+++ test-profile.el
@@ -1,7 +1,10 @@
 (advice-add #'x-apply-session-resources :override #'ignore)
 (setq make-backup-files nil
       auto-save-default nil
-      visible-bell t)
+      visible-bell t
+      warning-minimum-level :debug
+      warning-minimum-log-level :debug)
+(toggle-debug-on-error)
 
-(load-file "dracula-theme.el")
+(load "dracula-theme")
 (load-theme 'dracula t)
