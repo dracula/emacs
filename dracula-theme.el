6f6b6408a970bd9324f1882ea297af416f808891
Étienne Pflieger <etienne@pflieger.bzh>
2026-07-01 22:46:27 +0200

    Change format around colors definition

--- dracula-theme.el
+++ dracula-theme.el
@@ -179,27 +179,39 @@ read it before opening a new issue about your will.")
 
 ;; Assigment form: VARIABLE COLOR [256-COLOR [TTY-COLOR]]
 (let ((colors '(;; Upstream theme color
-                (dracula-bg      "#282a36" "unspecified-bg" "unspecified-bg") ; official background
-                (dracula-fg      "#f8f8f2" "#ffffff" "brightwhite") ; official foreground
-                (dracula-current "#44475a" "#303030" "brightblack") ; official current-line/selection
-                (dracula-region  "#454759" "#303030" "brightblack") ; dracula-current with fake 75% alpha
-                (dracula-comment "#6272a4" "#5f5faf" "blue")        ; official comment
-                (dracula-cyan    "#8be9fd" "#87d7ff" "brightcyan")  ; official cyan
-                (dracula-green   "#50fa7b" "#5fff87" "green")       ; official green
-                (dracula-orange  "#ffb86c" "#ffaf5f" "brightred")   ; official orange
-                (dracula-pink    "#ff79c6" "#ff87d7" "magenta")     ; official pink
-                (dracula-purple  "#bd93f9" "#af87ff" "brightmagenta") ; official purple
-                (dracula-red     "#ff5555" "#ff8787" "red")         ; official red
-                (dracula-yellow  "#f1fa8c" "#ffff87" "yellow")      ; official yellow
+                ;; official background
+                (dracula-bg      "#282a36" "unspecified-bg" "unspecified-bg")
+                ;; official foreground
+                (dracula-fg      "#f8f8f2" "#ffffff" "white")
+                ;; official current-line/selection
+                (dracula-current "#44475a" "#303030" "brightblack")
+                ;; dracula-current with fake 75% alpha
+                (dracula-region  "#454759" "#303030" "brightblack")
+                ;; official comment
+                (dracula-comment "#6272a4" "#5f5faf" "blue")
+                ;; official cyan
+                (dracula-cyan    "#8be9fd" "#87d7ff" "brightcyan")
+                ;; official green
+                (dracula-green   "#50fa7b" "#5fff87" "green")
+                ;; official orange
+                (dracula-orange  "#ffb86c" "#ffaf5f" "brightred")
+                ;; official pink
+                (dracula-pink    "#ff79c6" "#ff87d7" "magenta")
+                ;; official purple
+                (dracula-purple  "#bd93f9" "#af87ff" "brightmagenta")
+                ;; official red
+                (dracula-red     "#ff5555" "#ff8787" "red")
+                ;; official yellow
+                (dracula-yellow  "#f1fa8c" "#ffff87" "yellow")
                 ;; Other colors
-                (bg2             "#373844" "#121212" "brightblack")
-                (bg3             "#565761" "#444444" "brightblack")
-                (fg2             "#e2e2dc" "#e4e4e4" "brightwhite")
-                (fg3             "#ccccc7" "#c6c6c6" "white")
-                (fg4             "#b6b6b2" "#b2b2b2" "white")
-                (dark-red        "#880000" "#870000" "red") ; 40% darker
-                (dark-green      "#037a22" "#00af00" "green") ; 40% darker
-                (dark-blue       "#0189cc" "#0087ff" "brightblue")))
+                (bg2        "#373844" "#121212" "brightblack")
+                (bg3        "#565761" "#444444" "brightblack")
+                (fg2        "#e2e2dc" "#e4e4e4" "brightwhite")
+                (fg3        "#ccccc7" "#c6c6c6" "white")
+                (fg4        "#b6b6b2" "#b2b2b2" "white")
+                (dark-red   "#880000" "#870000" "red") ; 40% darker official variant
+                (dark-green "#037a22" "#00af00" "green") ; 40% darker official variant
+                (dark-blue  "#0189cc" "#0087ff" "brightblue")))
       (faces '(;; default / basic faces
                (cursor :background ,fg3)
                (default :background ,dracula-bg :foreground ,dracula-fg)
